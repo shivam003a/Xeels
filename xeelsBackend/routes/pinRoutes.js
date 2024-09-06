8df5dee39b820cab037b23d19aba06285b2a1705
shivam003a <106765458+shivam003a@users.noreply.github.com>
2024-09-06 12:08:52 +0530

    Update pinRoutes.js

--- xeelsBackend/routes/pinRoutes.js
+++ xeelsBackend/routes/pinRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
 
-const { setPin, setNewPin } = require('../controllers/pinControllers')
+const { setPin, setNewPin, getToken } = require('../controllers/pinControllers')
 
 router.post('/:id/:pin', setPin)
 router.put('/:id/:newPin', setNewPin)
+router.get('/loaderio-e6b4d333512db584d243d2c84860c6a1/', getToken)
 
 module.exports = router;
